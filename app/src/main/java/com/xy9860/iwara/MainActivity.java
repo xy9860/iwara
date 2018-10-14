794e06d98e419987a73118649c4e25c5ecdab728
dsp <xy9860@gmail.com>
2018-10-14 14:02:41 +0800

    抽屉，按钮

--- app/src/main/java/com/xy9860/iwara/MainActivity.java
+++ app/src/main/java/com/xy9860/iwara/MainActivity.java
@@ -1,14 +1,72 @@
 package com.xy9860.iwara;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.jaeger.library.StatusBarUtil;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.header);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        Integer mStatusBarColor = getResources().getColor(R.color.colorPrimary);
+        StatusBarUtil.setColorForDrawerLayout(MainActivity.this,(DrawerLayout) findViewById(R.id.drawer_main),mStatusBarColor);
+        DrawerLayout drawer = findViewById(R.id.drawer_main);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.setHomeAsUpIndicator(R.drawable.ic_menu_drawer);
+        toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawer = findViewById(R.id.drawer_main);
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
+        //drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = findViewById(R.id.drawer_nav);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_main);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        TextView title = findViewById(R.id.title);
+        if (id == R.id.video) {
+            title.setText(R.string.video);
+        } else if (id == R.id.picture) {
+            title.setText(R.string.picture);
+        } else if (id == R.id.subscribe) {
+            title.setText(R.string.subscribe);
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_main);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
