e4b9b80db7ad49ee2b5e81e7972fd3ebe56ee494
dsp <xy9860@gmail.com>
2018-10-16 01:32:33 +0800

    添加新Activity，数据传递，设置切换动画

--- app/src/main/java/com/xy9860/iwara/ShowItem.java
+++ app/src/main/java/com/xy9860/iwara/ShowItem.java
@@ -18,6 +18,7 @@ public class ShowItem extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finish();
+        /*fsaffs*/
         overridePendingTransition(R.anim.index_in,R.anim.show_item_out);
     }
 }
